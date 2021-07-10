75ebecfebb33fc14a46af9658305db355753ea8e
honeygoct <hiroki.honey@gmail.com>
2021-07-10 10:52:09 +0900

    新規登録、ログイン、ログアウト、エラーメッセージまで実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,14 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    Item.destroy
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_06_20_010752) do
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
     t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
