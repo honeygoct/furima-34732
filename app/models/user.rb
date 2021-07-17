5ef8e96d4d4f95fa9af77a5ca8f61501f2007120
honeygoct <hiroki.honey@gmail.com>
2021-07-17 11:25:23 +0900

    テストコードの記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,20 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :birthday, presence:true
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々]+\z/, message: 'には全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'には全角カタカナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+
   has_many :items
   has_many :orders
 
