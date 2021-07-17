27e7ef0b61c1bebbc3db54c6639a5bfb037d6d80
honeygoct <hiroki.honey@gmail.com>
2021-07-17 18:41:41 +0900

    Active storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,4 +12,10 @@ class ItemsController < ApplicationController
     Item.destroy
   end
 
+  private
+
+  def message_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,18 @@
 class Item < ApplicationRecord
   #validates :user, presence: true, foreign_key: true
-  validates :name, presence: true
-  validates :text, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_location_id, presence: true
-  validates :shipping_days_id, presence: true
-  validates :price, presence: true
-
-  #belongs_to :user
+  
+  with_options presence: true do
+  validates :name
+  validates :text
+  validates :category_id
+  validates :status_id
+  validates :prefecture_id
+  validates :shipping_location_id
+  validates :shipping_days_id
+  validates :price
+  end
+  
+  belongs_to :user
   #has_one :order
+  has_one_attached :image
 end
