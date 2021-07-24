42da51abf157a243dade444701cbbfd2925fcbb2
honeygoct <hiroki.honey@gmail.com>
2021-07-24 10:35:48 +0900

    商品情報編集機能②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show,]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+
 
   def index
     @items = Item.all.order("created_at ASC")
@@ -20,20 +22,20 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end 
 
    def edit
-    @item = Item.find(params[:id])
-    if current_user.id == @item.user_id
-      render :edit
-    else
+    if current_user.id =! @item.user_id
       redirect_to root_path
     end
    end
 
    def update
-    @item = Item.find(params[:id])
+    if current_user.id =! @item.user_id
+      redirect_to root_path
+    end
+    
     if @item.update(item_params)
       redirect_to action: :show
     else
@@ -52,4 +54,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image,:name,:explain,:category_id,:status_id,:shipping_fee_id,:prefecture_id,:shipping_days_id,:price).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
