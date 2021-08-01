dc0e774d88f9f19fc2902cb354b23725ba92e310
honeygoct <hiroki.honey@gmail.com>
2021-08-01 21:25:33 +0900

    商品購入機能③

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,20 +2,4 @@ class Address < ApplicationRecord
   belongs_to :order
 
 
-  #extend ActiveHash::Associations::ActiveRecordExtensions
-  #belongs_to :prefecture
-
-  #with_options presence: true do
-    #validates :postal_code
-    #validates :city
-    #validates :house_number
-    #validates :phone_number
-  #end
-
-
-  #with_options numericality: { other_than: 1 , message: "must be other than 1"}  do
-    #validates :prefecture_id
-  #end
-
-
 end
